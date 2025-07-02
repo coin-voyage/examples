ecaa52bcebf78413c9259cb2f0016f358e1b3472
Lars Sinke <magiclars@protonmail.com>
2025-07-02 10:55:23 +0200

    update `createPayOrder` to use `ApiClient`

--- nextjs-app/src/actions/pay-order.ts
+++ nextjs-app/src/actions/pay-order.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { createSalePayOrder, generateAuthorizationSignature } from "@coin-voyage/paykit/server";
+import { ApiClient } from "@coin-voyage/paykit/server";
 import { type PayOrder, type PayOrderMetadata } from "@coin-voyage/paykit/types";
 
 export interface PayOrderProps {
@@ -16,10 +16,17 @@ export async function createPayOrder(props: Partial<PayOrderProps>): Promise<Pay
     if (!process.env.NEXT_PUBLIC_COIN_VOYAGE_API_KEY || !process.env.COIN_VOYAGE_API_SECRET) {
         throw new Error('API key and secret are required')
     }
-    const signature = generateAuthorizationSignature(process.env.NEXT_PUBLIC_COIN_VOYAGE_API_KEY, process.env.COIN_VOYAGE_API_SECRET)
+
+    const isProduction = process.env.NEXT_PUBLIC_COIN_VOYAGE_ENVIRONMENT === 'production';
+    const apiClient = ApiClient({
+        apiKey: process.env.NEXT_PUBLIC_COIN_VOYAGE_API_KEY,
+        environment: isProduction ? 'production' : 'development',
+    });
+
+    const signature = apiClient.generateAuthorizationSignature(process.env.COIN_VOYAGE_API_SECRET)
 
     try {
-        const payOrder = await createSalePayOrder({
+        const payOrder = await apiClient.createSalePayOrder({
             destination_value_usd: props.valueUsd,
             metadata: props.metadata
         }, signature)
